88226fcc9725e6ddf0840c423ef5962db9b1d7d2
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-10-25 15:33:31 +0800

    base

--- reference.java
+++ reference.java
@@ -12,7 +12,7 @@ public class Reference {
 
     // f(n) = f(n-1) f(n-2)
     private static int fibonacci(int n) {
-        if (n<= 1) return n;
+        if (n<=1) return n;
         return fibonacci(n-1) + fibonacci(n-2);
     }
 
