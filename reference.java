e7b7f7a0c4dd9e1214b1293c656b0524ff905c2c
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-10-25 14:13:48 +0800

    fibonacci reference algo

--- reference.java
+++ reference.java
@@ -1,15 +1,25 @@
 public class Reference {
 
     public static void main(String[] args) {
-        int n = 6;
+        int n = 10;
+        int[] nFibTerms = new int[n];
 
-        System.out.println(fibonacci(n));
+        for (int i=0; i<nFibTerms.length; i++) {
+            nFibTerms[i] = fibonacci(i);
+        }
+        listArray(nFibTerms);
     }
 
     // f(n) = f(n-1) f(n-2)
-    private static long fibonacci(int n) {
+    private static int fibonacci(int n) {
         if (n<= 1) return n;
         return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    private static void listArray(int[] arr) {
+        for (int a:arr) {
+            System.out.println(a);
+        }
     }
 
 }
